83c9915d90e8b375f5233a14d8a62742e22010b4
hasdsd <114514@1919810.com>
2022-08-16 13:08:30 +0800

    代码生成器

---
+++ src/main/java/com/syes/syes_springboot/SyesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.syes.syes_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyesSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyesSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/syes/syes_springboot/common/Generator.java
@@ -0,0 +1,40 @@
+package com.syes.syes_springboot.common;
+
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+public class Generator {
+    public static void main(String[] args) {
+        FastAutoGenerator.create(
+                        "jdbc:mysql://hasdsd.cn:3307/syes",
+                        "root",
+                        "123456")
+                .globalConfig(builder -> {
+                    builder.author("hasd") // 设置作者
+                            .fileOverride() // 覆盖已生成文件
+                            .enableSwagger() // 开启 swagger 模式
+                            // 指定输出目录
+                            //直接右键复制项目根目录的绝对路径
+                            .outputDir("D:/CODE/syes_springboot/src/main/java/");
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.syes.syes_springboot") // 设置父包名
+                            // 设置mapperXml生成路径
+                            //直接右键复制项目mapper文件夹的绝对路径
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:/CODE/syes_springboot/src/main/resources/mapper"));
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("user")//设置表名
+                            .entityBuilder()    //entity前置，才能用lombok
+                            .enableLombok()  //使用lombok
+                            .controllerBuilder()
+                            .enableHyphenStyle(); //restMapping注解
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
