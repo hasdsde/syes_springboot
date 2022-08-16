3979330bf00a411ee5951337539bbe928e2c76ac
hasdsd <114514@1919810.com>
2022-08-16 19:11:04 +0800

    haha

--- src/main/java/com/syes/syes_springboot/controller/UserController.java
+++ src/main/java/com/syes/syes_springboot/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.syes.syes_springboot.controller;
 
+import com.syes.syes_springboot.entity.User;
+import com.syes.syes_springboot.mapper.UserMapper;
+import com.syes.syes_springboot.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
--- src/main/java/com/syes/syes_springboot/entity/User.java
+++ src/main/java/com/syes/syes_springboot/entity/User.java
@@ -2,14 +2,12 @@ package com.syes.syes_springboot.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author hasd
@@ -17,29 +15,23 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@ApiModel(value = "User对象", description = "")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("学号")
+
     private String number;
 
     private String nickname;
 
-    @ApiModelProperty("真实姓名")
     private String realname;
 
-    @ApiModelProperty("密码")
     private String password;
 
-    @ApiModelProperty("用户头像地址")
     private String avatar;
 
-    @ApiModelProperty("创建日期")
     private LocalDateTime createtime;
 
-    @ApiModelProperty("手机号")
     private String phone;
 
 
