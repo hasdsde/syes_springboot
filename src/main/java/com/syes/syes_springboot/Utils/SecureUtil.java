dcfee0254027bcb49d0c746e7d8c2c6d30d57bbb
sdadgz <pengchengma5@gmail.com>
2022-08-18 16:31:58 +0800

    交互

--- src/main/java/com/syes/syes_springboot/Utils/SecureUtil.java
+++ src/main/java/com/syes/syes_springboot/Utils/SecureUtil.java
@@ -3,16 +3,21 @@ package com.syes.syes_springboot.Utils;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class SecureUtil {
 
+    // 废弃的，过时的，无法处理大文件的
     public static String md5(MultipartFile file){
         // 面向cv编程，哈哈我是就是个废物
         try {
-            //获取文件的byte信息
+            //获取文件的byte信息，文件大了会报错
             byte[] uploadBytes = file.getBytes();
             // 拿到一个MD5转换器
             MessageDigest md5 = MessageDigest.getInstance("MD5");
@@ -23,5 +28,27 @@ public class SecureUtil {
 
         }
         return null;
+    }
+
+    public static String bigMD5(File file){
+        // 大文件报错byte是int设置长度，int超范围，哈哈我又偷懒去cv的
+        BigInteger bi = null;
+        try {
+            byte[] buffer = new byte[8192];
+            int len = 0;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+//            File f = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            while ((len = fis.read(buffer)) != -1) {
+                md.update(buffer, 0, len);
+            }
+            fis.close();
+            byte[] b = md.digest();
+            bi = new BigInteger(1, b);
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+        }
+        assert bi != null;
+        return bi.toString(16);
     }
 }
--- src/main/java/com/syes/syes_springboot/controller/FileController.java
+++ src/main/java/com/syes/syes_springboot/controller/FileController.java
@@ -49,30 +49,41 @@ public class FileController {
     @PostMapping("/upload")
     public Result upload(@RequestParam("file") MultipartFile file) {
 
+        // 测试用的map
+        Map<String, Object> map = new HashMap<>();
+
+        // 获取uuid
+        String uuid = IdUtil.fastSimpleUUID();
+
         // 获取需要上传数据库的属性
         String originalFilename = file.getOriginalFilename();
         String type = FileUtil.extName(originalFilename);
         String size = String.valueOf(file.getSize());
         String url = downloadPath;
-        String md5 = SecureUtil.md5(file);
 
-        // 获取uuid
-        String uuid = IdUtil.fastSimpleUUID();
+        // name是主键吧，为了不报错就加个判断吧
+        if (nameExists(originalFilename)) {
+            return Result.success("名字已经存在");
+        }
+
+        // 上传文件，创建file方便删除
+        String path = uploadPath + uuid + type;
+        uploadToServer(file, path);
+        java.io.File jFile = new java.io.File(path);
+
+        // 追加md5
+        String md5 = SecureUtil.bigMD5(jFile);
 
         // 文件去重
         url += uuid + type;
         String oldUrl = md5Exists(md5);
         if (oldUrl != null) { // 文件重复
             url = oldUrl;
-        } else {
-            // 文件不重复，上传文件
-            String path = uploadPath + uuid + type;
-            uploadToServer(file, path);
-        }
-
-        // name是主键吧，为了不报错就加个判断吧
-        if (nameExists(originalFilename)){
-            return Result.success("名字已经存在");
+            if (jFile.delete()) {
+                map.put("file", "重复文件，删除了，用旧的");
+            } else {
+                map.put("file", "重复文件，没删了，出大事了");
+            }
         }
 
         // 上传数据库
@@ -85,36 +96,19 @@ public class FileController {
         fileMapper.insert(saveFile);
 
         // 测试
-        Map<String, Object> map = new HashMap<>();
         map.put("originalFilename", originalFilename);
-        map.put("md5", md5);
         map.put("type", type);
-        map.put("uuid", uuid);
         map.put("size", size);
         map.put("url", url);
+        map.put("md5", md5);
+
+        map.put("uuid", uuid);
 
         return Result.success(map);
     }
 
-    //下载文件
-//    @GetMapping("/{Fileuuid}")
-//    public void download(@PathVariable("Fileuuid") String FileUUID, HttpServletResponse response) throws IOException {
-//        //根据UUID获取文件
-//        java.io.File file = new java.io.File(uploadPath + FileUUID);
-//        //设置输出流的格式
-//        ServletOutputStream os = response.getOutputStream();
-//        //标准表头
-//        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(FileUUID, "UTF-8"));
-//        response.setContentType("application/octet-stream");
-//
-//        //读取文件的字节流
-//        os.write(FileUtil.readBytes(file));
-//        os.flush();
-//        os.close();
-//    }
-
     // 上传到服务器
-    private boolean uploadToServer(MultipartFile file, String path) {
+    private void uploadToServer(MultipartFile file, String path) {
         if (!file.isEmpty()) {
             try {
                 file.transferTo(new java.io.File(path));
@@ -122,7 +116,6 @@ public class FileController {
                 throw new RuntimeException(e);
             }
         }
-        return true;
     }
 
     // 查询md5值
@@ -145,129 +138,4 @@ public class FileController {
         return files.size() > 0;
     }
 
-    /*
-    //    //获取配置
-    @Value("/$/{/files/./upload/./path}/") //加斜杠因为博客会认成代码块
-    private String fileUploadPath;
-
-
-
-
-    //标记修改删除和修改是否可用
-    @PutMapping("/put")
-    public Result MarkDeleted(@RequestBody File file) {
-        fileMapper.updateById(file);
-        return Result.success();
-    }
-
-    @GetMapping("/id/{id}")
-    public Result getFileById(@PathVariable("id") int id) {
-        File file = fileMapper.selectById(id);
-        return Result.success(file);
-    }
-
-    //分页查询
-    @GetMapping("/page/p")
-    public Result getUserByPage(@RequestParam("PageNumber") int PageNumber, @RequestParam("PageSize") int PageSize) {
-        // 设置开始页
-        int PageStart = (PageNumber - 1) * PageSize;
-
-        // 获取总数
-        QueryWrapper wrapper = new QueryWrapper();
-        int total = fileMapper.selectCount(wrapper);
-
-        // 查询
-        System.out.println("Pagestart:" + PageStart + "  PageSize" + PageSize);
-        List<File> result = fileMapper.slectByPage(PageStart, PageSize);
-
-        Map<String, Object> res = new HashMap<>();
-        res.put("total", total);
-        res.put("data", result);
-        return Result.success(res);
-    }
-
-    // 删除文件
-    @DeleteMapping("/{id}")
-    public Result deleteFileById(@PathVariable("id") int id, HttpServletResponse response) {
-        int i = fileMapper.deleteById(id);
-        return Result.success(i);
-    }
-
-    //上传文件
-    @PostMapping("/upload")
-    public String upload(@RequestParam MultipartFile file) throws IOException {
-        //获取名称
-        String originalFilename = file.getOriginalFilename();
-        //获取类型
-        String type = FileUtil.extName(originalFilename);
-        //获取大小
-        long size = file.getSize();
-        //判断配置文件的目录是否存在
-        java.io.File uploadParentFile = new java.io.File(fileUploadPath);
-
-        if (!uploadParentFile.exists()) {
-            uploadParentFile.mkdirs();
-        }
-
-        //定义文件唯一标识位
-        String uuid = IdUtil.fastSimpleUUID();
-        //UUID，这样方便后续下载
-        String FileUUID = uuid + StrUtil.DOT + type;
-
-        //文件路径  UUID+点+文件类型
-        java.io.File uploadFile = new java.io.File(fileUploadPath + FileUUID);
-
-        //下面一大片操作目的是：遇到重复文件时，数据库增加数据，服务器储存不变
-        String md5;
-        String url;
-        file.transferTo(uploadFile);
-        md5 = SecureUtil.md5(uploadFile);
-
-
-        File dbFile = getFileByMd5(md5);
-        if (dbFile != null) {
-            url = dbFile.getUrl();
-            uploadFile.delete();
-        } else {
-            url = "http://192.168.0.123:8080/file/" + FileUUID;
-        }
-        //给数据库添加记录
-        File saveFile = new File();
-        saveFile.setName(originalFilename);
-        saveFile.setType(type);
-        saveFile.setSize(String.valueOf(size)); // 参数类型不符，转一下
-        saveFile.setUrl(url);
-        saveFile.setMd5(md5);
-        fileMapper.insert(saveFile);
-        //返回了文件下载链接
-        return url;
-    }
-
-    //下载文件
-    @GetMapping("/{Fileuuid}")
-    public void download(@PathVariable("Fileuuid") String FileUUID, HttpServletResponse response) throws IOException {
-        //根据UUID获取文件
-        java.io.File file = new java.io.File(fileUploadPath + FileUUID);
-        //设置输出流的格式
-        ServletOutputStream os = response.getOutputStream();
-        //标准表头
-        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(FileUUID, "UTF-8"));
-        response.setContentType("application/octet-stream");
-
-        //读取文件的字节流
-        os.write(FileUtil.readBytes(file));
-        os.flush();
-        os.close();
-    }
-
-    //通过MD5查询数据
-    private File getFileByMd5(String md5) {
-        QueryWrapper<File> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("md5", md5);
-        List<File> files = fileMapper.selectList(queryWrapper);
-        return files.size() == 0 ? null : files.get(0);
-    }
-
-
-     */
 }
