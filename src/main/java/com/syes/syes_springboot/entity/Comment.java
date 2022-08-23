c18277e4fb2819a1296fa71eeb8b65674c7c93cc
hasdsde <1259305706@qq.com>
2022-08-23 23:01:41 +0800

    对comment类做了少许修改

--- src/main/java/com/syes/syes_springboot/entity/Comment.java
+++ src/main/java/com/syes/syes_springboot/entity/Comment.java
@@ -1,16 +1,14 @@
 package com.syes.syes_springboot.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -29,17 +27,20 @@ public class Comment implements Serializable {
 
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
-
+    //物品id
     private Integer itemid;
-
+    //上传者id
     private String userid;
-
+    //创建时间
     private LocalDateTime createtime;
 
+    //评论内容
     private String content;
 
+    //在哪个评论之下
     private Integer tocommentid;
 
     // 用户
+    @TableField(exist = false)
     private User user;
 }
