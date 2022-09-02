4405155cb37aeb78bcca04ce822341d0e409d2b1
xsheng <2838428014@qq.com>
2022-09-02 09:34:43 +0800

    轮播图

---
+++ src/main/java/com/syes/syes_springboot/service/impl/RollimgServiceImpl.java
@@ -0,0 +1,20 @@
+package com.syes.syes_springboot.service.impl;
+
+import com.syes.syes_springboot.entity.Rollimg;
+import com.syes.syes_springboot.mapper.RollimgMapper;
+import com.syes.syes_springboot.service.IRollimgService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author hasdsd
+ * @since 2022-09-02
+ */
+@Service
+public class RollimgServiceImpl extends ServiceImpl<RollimgMapper, Rollimg> implements IRollimgService {
+
+}
