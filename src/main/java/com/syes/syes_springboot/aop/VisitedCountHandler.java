c8ad5d666b179654614d9d88e7f1ca9c268d6b8c
hasdsde <1259305706@qq.com>
2022-09-12 23:28:39 +0800

    我会了

--- src/main/java/com/syes/syes_springboot/aop/VisitedCountHandler.java
+++ src/main/java/com/syes/syes_springboot/aop/VisitedCountHandler.java
@@ -1,9 +1,15 @@
 package com.syes.syes_springboot.aop;
 
+import com.syes.syes_springboot.Utils.IdUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 @Aspect
@@ -11,6 +17,11 @@ public class VisitedCountHandler {
 
     @After(value = "execution(*  com.syes.syes_springboot.controller.ItemController.itemById(..)) )")
     public void before(JoinPoint joinPoint) throws Throwable {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+        String id = IdUtil.getId(request);
+        System.out.println("userid=" + id);
         Object[] args = joinPoint.getArgs();
         Object itemid = args[0].toString();
         Object header = args[0];
