fd183c11c66dbf106c5a0455e59e592f70576cfa
hasdsde <1259305706@qq.com>
2022-09-19 23:21:17 +0800

    提交

--- src/main/java/com/syes/syes_springboot/aop/VisitedCountHandler.java
+++ src/main/java/com/syes/syes_springboot/aop/VisitedCountHandler.java
@@ -1,5 +1,8 @@
 package com.syes.syes_springboot.aop;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.syes.syes_springboot.Utils.IdUtil;
+import com.syes.syes_springboot.entity.Userhistory;
 import com.syes.syes_springboot.mapper.UserhistoryMapper;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -7,6 +10,12 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 //设置浏览量
 @Component
@@ -20,29 +29,29 @@ public class VisitedCountHandler {
     //触发方法时，获取itemid
     @After(value = "execution(*  com.syes.syes_springboot.controller.ItemController.itemById(..)) )")
     public void before(JoinPoint joinPoint) throws Throwable {
-//        //获取参数
-//        Object[] args = joinPoint.getArgs();
-//        Object itemid = args[0].toString();
-//        //将获取的参数传递到Redis
-//        redisTemplate.opsForHash().increment("item", itemid, 1);
-//        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
-//        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
-//        HttpServletRequest request = sra.getRequest();
-//        String id = IdUtil.getId(request);
-//        //添加历史记录到mysql，我服了怎么这么复杂
-//        QueryWrapper<Userhistory> wrapper = new QueryWrapper<>();
-//        wrapper.eq("itemid", itemid);
-//        wrapper.eq("userid", id);
-//        Long count = userhistoryMapper.selectCount(wrapper);
-//        Userhistory userhistory = new Userhistory();
-//        userhistory.setCreatetime(LocalDateTime.now());
-//        userhistory.setUserid(id);
-//        userhistory.setItemid(Integer.valueOf(itemid.toString()));
-//        if (count > 0) {
-//            userhistoryMapper.update(userhistory, wrapper);
-//        } else {
-//            userhistoryMapper.insert(userhistory);
-//        }
+        //获取参数
+        Object[] args = joinPoint.getArgs();
+        Object itemid = args[0].toString();
+        //将获取的参数传递到Redis
+        redisTemplate.opsForHash().increment("item", itemid, 1);
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+        String id = IdUtil.getId(request);
+        //添加历史记录到mysql，我服了怎么这么复杂
+        QueryWrapper<Userhistory> wrapper = new QueryWrapper<>();
+        wrapper.eq("itemid", itemid);
+        wrapper.eq("userid", id);
+        Long count = userhistoryMapper.selectCount(wrapper);
+        Userhistory userhistory = new Userhistory();
+        userhistory.setCreatetime(LocalDateTime.now());
+        userhistory.setUserid(id);
+        userhistory.setItemid(Integer.valueOf(itemid.toString()));
+        if (count > 0) {
+            userhistoryMapper.update(userhistory, wrapper);
+        } else {
+            userhistoryMapper.insert(userhistory);
+        }
 
 
     }
